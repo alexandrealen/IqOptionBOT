fa73eb3960f38c7e53352395c629f370cc0ee8b8
alexandre_alencar <alexandre.alencar@intelitrader.com.br>
2021-01-31 22:09:20 -0300

    melhora a user experience e completa a versão 1.0 do bot

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import time, threading, schedule
 #
 class TelaLogin:
     def __init__(self):
+        sg.theme("Reddit")
         layout = [
                   [sg.Text('Login'), sg.Input()],      
                   [sg.Text('Senha'), sg.Input(password_char = "*")],      
@@ -19,6 +20,7 @@ class TelaLogin:
         self.event, self.values = window.Read()
 
         while self.values[0] == "" or self.values[1] == "":
+            sg.theme("Reddit")
             layout = [
                       [sg.Text('COLOCA O LOGIN E A SENHA ANIMAL')],
                       [sg.Text('Login'), sg.Input()],      
@@ -34,12 +36,13 @@ class TelaLogin:
 #
 class Tela:
     def __init__(self):
-        symbols = ["EURUSD", "EURJPY", "EURNZD", "EURAUD", "USDJPY", "AUDUSD", "AUDJPY", "NZDUSD", "EURUSD-OTC"]
+        sg.theme("Reddit")
+        symbols = ["EURUSD", "EURJPY", "EURNZD", "EURAUD", "USDJPY", "AUDUSD", "AUDJPY", "NZDUSD", "EURUSD-OTC", "EURJPY-OTC", "EURNZD-OTC", "EURAUD-OTC", "USDJPY-OTC", "AUDUSD-OTC", "AUDJPY-OTC", "NZDUSD-OTC"]
         tempo = ["1 min", "5 min", "15 min"]
         opt = ["call", "put"]
         layout = [
                   [sg.Text('Valor'), sg.Input("5", key = "value-input")],
-                  [sg.Checkbox('Definir horário?', key = "hour"), sg.Input("example: 01:23, 02:00, 14:30, 23:01", key = "hour-input")],
+                  [sg.Checkbox('Definir horário?', key = "hour", default=True), sg.Input("01:59:58", key = "hour-input")],
                   [sg.Checkbox('Conta Real', key = "account")],
                   [sg.OptionMenu(values = symbols, key = "symbol"), sg.OptionMenu(values = tempo, key = "time"), sg.OptionMenu(values = opt, key = "opt")],
                   [sg.Output(size = (60,5), echo_stdout_stderr = True)],
@@ -60,14 +63,15 @@ def Buy(quantity, symbol, opt, expireTime):
     check, id = api.buy(quantity, symbol, opt, expireTime)
     if check:
         print("Lucro/preju: %.2f" % api.check_win_v3(id))
-        return
     else:
-        print("Operação falhou :(")
-        return
+        print("Operação falhou, verifique se inseriu os dados corretamente e se o simbolo o qual deseja apostar está aberto")
+    print("Para operar novamente, feche e abra o programa :)")
 
 def operar(value, symbol , opt, expireTime):  
     print("Deu a hora menó, iniciando a operação")
-    threading.Thread(target=Buy, args=(value, symbol , opt, expireTime, )).start()
+    t2 = threading.Thread(target=Buy, args=(value, symbol , opt, expireTime, ))
+    t2.daemon = True
+    t2.start()
 
 def agendar(hour_input, value, symbol , opt, expireTime):
     schedule.every().day.at(hour_input).do(operar, value, symbol , opt, expireTime)
@@ -79,8 +83,10 @@ def agendar(hour_input, value, symbol , opt, expireTime):
 #Começa instanciando a tela de login e fazendo as verificações
 #
 telaLogin = TelaLogin()
-#api = IQ_Option(str(telaLogin.values[0]), str(telaLogin.values[1]))
-api = IQ_Option("alexandrealen03@gmail.com","alexandrealen03@gmail.com")
+if telaLogin.event == sg.WIN_CLOSED:
+    telaLogin.window.close()
+    exit()
+api = IQ_Option(str(telaLogin.values[0]), str(telaLogin.values[1]))
 api.connect()
 telaLogin.window.close()
 
@@ -101,6 +107,9 @@ while api.check_connect() == False:
 #Se tudo der certo no login a tela principal é instanciada 
 #
 tela = Tela()
+if tela.event == sg.WIN_CLOSED:
+    tela.window.close()
+    exit()
 
 if tela.values["account"]:
     conta = "REAL"
@@ -125,8 +134,27 @@ if tela.values["hour"]:
     symbol = tela.values["symbol"]
     opt = tela.values["opt"]
     expireTime = tela.values["time"]
-    threading.Thread(target=agendar, args=(hour_input, value, symbol , opt, expireTime, )).start()
+    t1 = threading.Thread(target=agendar, args=(hour_input, value, symbol , opt, expireTime, ))
+    t1.daemon = True
+    t1.start()
     tela.window.Element("submit").Update(disabled = True)
     while True:
-        tela.window.Read()
-    
+        event, values = tela.window.Read()
+        if event == sg.WIN_CLOSED:
+            exit()
+else:
+    print("Iniciando operação agora")
+    value = float(tela.values["value-input"])
+    symbol = tela.values["symbol"]
+    opt = tela.values["opt"]
+    expireTime = tela.values["time"]
+    t1 = threading.Thread(target=Buy, args=(value, symbol , opt, expireTime, ))
+    t1.daemon = True
+    t1.start()
+    tela.window.Element("submit").Update(disabled = True)
+    while True:
+        event, values = tela.window.Read()
+        if event == sg.WIN_CLOSED:
+            break
+
+exit()
